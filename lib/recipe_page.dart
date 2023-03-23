55794b514d055a964b276ad61e0ba695c18a2b86
Abdullah <95099545+abdullah-9211@users.noreply.github.com>
2023-03-23 19:11:44 +0500

    added instructions and review stars

--- lib/recipe_page.dart
+++ lib/recipe_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'constants.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class RecipePage extends StatefulWidget {
   final List<String> ingredients;
@@ -15,6 +16,8 @@ class RecipePage extends StatefulWidget {
 
 class _RecipePageState extends State<RecipePage> {
   bool _showAllIngredients = false;
+  bool _showAllSteps = false;
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,13 +39,38 @@ class _RecipePageState extends State<RecipePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(
-                "by Tasty",
-                style: TextStyle(
-                  color: textColor,
-                  fontFamily: bodyFont,
-                  fontSize: bodyFontSize,
-                ),
+              Row(
+                  children: [
+                    Container(
+                      child: Text(
+                        "by Tasty",
+                        style: TextStyle(
+                          color: textColor,
+                          fontFamily: bodyFont,
+                          fontSize: bodyFontSize,
+
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: RatingBar.builder(
+                        initialRating: 3,
+                        minRating: 1,
+                        itemSize: 23.0,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                        itemBuilder: (context, _) => Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        onRatingUpdate: (rating) {
+                          print(rating);
+                        },
+                      ),
+                    ),
+                  ]
               ),
               SizedBox(
                 height: 20.0,
@@ -68,6 +96,42 @@ class _RecipePageState extends State<RecipePage> {
                       onPressed: () {
                         setState(() {
                           _showAllIngredients = true;
+                        });
+                      },
+                      child: const Text(
+                        'See more...',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15.0),
+                      ),
+                    ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              const Text(
+                'Instructions',
+                style: subHeadingStyle,
+              ),
+              SizedBox(height: 10.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ...widget.steps
+                      .take(_showAllSteps ? widget.steps.length : 3)
+                      .map(
+                        (step) => Text(
+                      '\u2022 $step',
+                      style: bodyStyle,
+                    ),
+                  ),
+                  if (!_showAllSteps)
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _showAllSteps = true;
                         });
                       },
                       child: const Text(
